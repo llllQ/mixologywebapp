2c85e55ef3afafae38f0f58b02a8071607afc04c
llllQ <matthew.penlington@gmail.com>
2019-04-30 11:02:03 +0100

    initial speech recognition

---
+++ scripts/script.js
@@ -0,0 +1,82 @@
+// chrome support
+var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
+var SpeechGrammarList = SpeechGrammarList || webkitSpeechGrammarList;
+var SpeechRecognitionEvent = SpeechRecognitionEvent || webkitSpeechRecognitionEvent;
+
+
+// Defining grammar
+var drinks = [ 'margarita' , 'long island iced tea' , 'pina colada', 'woo woo', 'singapore sling', 'canada goose', 'sex on the beach', 'rumball', 'old fashioned', 'negroni'];
+var grammar = '#JSGF V1.0; grammar drinks; public <drink> = ' + drinks.join(' | ') + ' ;'
+
+// recongition controls
+var recognition = new SpeechRecognition();
+var speechRecognitionList = new SpeechGrammarList();
+
+speechRecognitionList.addFromString(grammar, 1);
+
+recognition.grammars = speechRecognitionList;
+//recognition.continuous = false;
+// Set language of voice detection
+recognition.lang = 'en-US'; 
+// Define type of results, final or interim
+recognition.interimResults = false;
+// Number of potential matches that should be returned per result
+recognition.maxAlternatives = 1;
+
+// document.body.onclick = function(){
+//   console.log("SPEAK!");
+//     recognition.start();
+// }
+
+function runSpeech(){
+  console.log("SPEAK!");
+    recognition.start();
+}
+
+/**
+ *Function fires when a successful voice recognition result is received
+ *
+ * @param {*} event
+ */
+recognition.onresult = function(event) {
+    var last = event.results.length - 1;
+    var drink = event.results[last][0].transcript;
+    console.log('Result received: ' + drink + '.');
+    console.log('Confidence: ' + event.results[0][0].confidence);
+    drinkRequest(drink);
+    
+  }
+
+  recognition.onspeechend = function() {
+    recognition.stop();
+  }
+
+  recognition.onnomatch = function(event) {
+    console.log('I didnt recognise that color.');
+  }
+
+  recognition.onerror = function(event) {
+    console.log('Error occurred in recognition: ' + event.error);
+  }
+
+  function drinkRequest(drink){
+    const Http = new XMLHttpRequest();
+    var parameters = drink;
+    const url ="https://www.thecocktaildb.com/api/json/v1/1/search.php?s=" + parameters;
+
+    console.log("request url: "+ url);
+    Http.open("GET", url);
+    Http.send();
+
+      Http.onreadystatechange = function() {
+        if (this.status == 200){
+            const data = Http.responseText;
+            console.log(data);
+
+
+        }
+    }
+
+
+
+  }
